6ae22ca236b33cd307cf55217a3a08ac2c1f6906
KyleRattet <krattet@gmail.com>
2015-07-22 06:51:05 -0600

    added sum-all button, logic and dom

--- js/dom.js
+++ js/dom.js
@@ -6,3 +6,23 @@ Hints for the first problem:
     - passes all of its options to the `sum` function defined in `logic.js`
     - takes the value returned by the `sum` function and displays it on the page
 */
+
+////Variables Assigned to DOM locations
+//answer -- DIV where the answers are inserted
+var answers = document.getElementById('answer');
+
+//options list to access prices
+var options = document.getElementsByTagName('option');
+
+//sum all button
+var sumAllButton = document.getElementById('sum-all');
+
+
+
+//Event handlder functions to make buttons do things
+
+//1. sum all button
+///first line ==>> where to listen from, 2nd line ==> what to do when clicked and where to insert it
+sumAllButton.addEventListener("click", function (){
+  answers.innerHTML = sum(options);
+  });
--- js/logic.js
+++ js/logic.js
@@ -6,3 +6,16 @@ Hints for the first problem:
     - returns the calculated sum of all of the values of all of the options passed in
 NOTE: use a `for` loop to make this happen.
 */
+
+//will be an array of all the selected values that need to get pushed to sum
+
+
+
+//1. sumfunction
+function sum (options) {
+  var total = 0;
+  for (var i = 0; i < options.length; i++) {
+    total += + options[i].value;
+  };
+  return total;
+}
