b8c4918e5765d5e8399968fb1cbbbe9eedb0a7ba
KyleRattet <krattet@gmail.com>
2015-07-23 18:15:03 -0600

    added print all names button

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,4 @@
+if (document.URL === "file:///Users/KyleRattet/Documents/Homework/js-iteration/index.html") {
 ////Variables Assigned to DOM locations
 //answer -- DIV where the answers are inserted
 var answers = document.getElementById('answer');
@@ -38,6 +39,7 @@ averageSelectedButton.addEventListener("click", function () {
   answers.innerHTML = averageSelected(options);
 });
 
+} else if (document.URL === "file:///Users/KyleRattet/Documents/Homework/js-iteration/names.html") {
 /////names
 //assign variables to get items
 
@@ -51,15 +53,19 @@ var namesList = document.getElementsByTagName('textarea')[0].value;
 var printAllNamesButton = document.getElementById('all-names');
 
 //test to print all names button
-var finalList = stringToArray(namesList);
+
 
 printAllNamesButton.addEventListener("click", function () {
-  var li = document.createElement('li');
+  var finalArrayList = stringToArray(namesList);
+
+  for (var i = 0; i < finalArrayList.length; i++) {
+    var li = document.createElement('li');
   var ul = document.getElementById('nameAnswers');
-  ul.appendChild(li).innerHTML = finalList;
+  ul.appendChild(li).innerHTML = finalArrayList[i];
+  }
 });
 
-
+}
 ///test 2
 
 
@@ -68,14 +74,14 @@ printAllNamesButton.addEventListener("click", function () {
 // var ul = document.getElementById('nameAnswers');
 // ul.appendChild(li)
 
-function createElement () {
-  var namesArrary = stringToArray(namesList);
-  for (var i = 0; i < namesArray.length; i++) {
-    var li = document.createElement('li');
-    var ul = document.getElementById('nameAnswers');
-    ul.appendChild(li).innerHTML = stringToArray(namesList)[i] + ;
-//   };
-// }
+// function createElement () {
+//   var namesArrary = stringToArray(namesList);
+//   for (var i = 0; i < namesArray.length; i++) {
+//     var li = document.createElement('li');
+//     var ul = document.getElementById('nameAnswers');
+//     ul.appendChild(li).innerHTML = stringToArray(namesList)[i] + ;
+// //   };
+// // }
 
 //try to add one name in new li
 // printAllNamesButton.addEventListener("click", function  () {
@@ -96,8 +102,112 @@ function createElement () {
 // };
 
 
+// // if (document.URL = "file:///Users/KyleRattet/Documents/Homework/js-iteration/index.html") {
+// ////Variables Assigned to DOM locations
+// //answer -- DIV where the answers are inserted
+// var answers = document.getElementById('answer');
+// //options list to access prices
+// var options = document.getElementsByTagName('option');
+// //sum all button
+// var sumAllButton = document.getElementById('sum-all');
+// ///count selected button
+// var countSelectedButton = document.getElementById('count-selected');
+// //sum selected button
+// var sumSelectedButton = document.getElementById('sum-selected');
+// //average all button
+// var averageAllButton = document.getElementById('average-all');
+// //average selected button
+// var averageSelectedButton = document.getElementById('average-selected');
+
+// //Event handlder functions to make buttons do things
+
+// //1. sum all button
+// ///first line ==>> where to listen from, 2nd line ==> what to do when clicked and where to insert it
+// sumAllButton.addEventListener("click", function (){
+//   answers.innerHTML = sum(options);
+//   });
+// //2. count selected button
+// countSelectedButton.addEventListener("click", function () {
+//   answers.innerHTML = countSelected (options);
+// });
+// //3. sum selected button
+// sumSelectedButton.addEventListener("click", function () {
+//   answers.innerHTML = sumSelected (options);
+// });
+// //4.average all button
+// averageAllButton.addEventListener("click", function () {
+//   answers.innerHTML = averagePrices(options);
+// });
+// //5. averageSelected button
+// averageSelectedButton.addEventListener("click", function () {
+//   answers.innerHTML = averageSelected(options);
+// });
+
+
+// // else (document.URL = "file:///Users/KyleRattet/Documents/Homework/js-iteration/names.html") {
+
+
+// //end of prices
+// /////names
+// //assign variables to get items
+
+// //nameAnswers - where the selected names get printed
+// var nameAnswers = document.getElementById('nameAnswers');
+
+// //location of the all the names
+// var namesList = document.getElementsByTagName('textarea')[0].value;
+
+// //print all names button
+// var printAllNamesButton = document.getElementById('all-names');
+
+// //test to print all names button
+// var finalList = stringToArray(namesList);
+
+// printAllNamesButton.addEventListener("click", function () {
+//   var li = document.createElement('li');
+//   var ul = document.getElementById('nameAnswers');
+//   ul.appendChild(li).innerHTML = finalList;
+// });
+
+
+// ///test 2
+
+
+// //iterate through array, then create new li, add index value to that li
+// // var li = document.createElement('li');
+// // var ul = document.getElementById('nameAnswers');
+// // ul.appendChild(li)
+
+// function createElement () {
+//   var namesArrary = stringToArray(namesList);
+//   for (var i = 0; i < namesArray.length; i++) {
+//     var li = document.createElement('li');
+//     var ul = document.getElementById('nameAnswers');
+//     ul.appendChild(li).innerHTML = stringToArray(namesList);
+// //   };
+// // }
+
+// //try to add one name in new li
+// // printAllNamesButton.addEventListener("click", function  () {
+// //   var li = document.createElement('li');
+// //   var ul = document.getElementById('nameAnswers');
+// //   ul.appendChild(li) = namesListArray[0];
+// // });
+
+
+// // var li = document.createElement('li');
+// // var ul = document.getElementById('nameAnswers');
+// // ul.appendChild(li) = namesListArray[0]
+
+// // for (var i = 0; i < stringToArray(namesList).length; i++) {
+// //   var li = document.createElement('li');
+// //   var ul = document.getElementById('nameAnswers');
+// //   ul.appendChild(li).innerHTML = stringToArray(namesList)[i];
+// // };
 
 
 
+
+// // }
 
 
--- js/logic.js
+++ js/logic.js
@@ -4,7 +4,7 @@ function sum (options) {
   var total = 0;
   for (var i = 0; i < options.length; i++) {
     total += parseInt(options[i].value);
-  };
+  }
   return total;
 }
 //2.  count the selected prices
@@ -12,9 +12,9 @@ function countSelected (options){
   var count = 0;
   for (var i = 0; i < options.length; i++) {
     if (options[i].selected === true) {
-      count ++
+      count ++;
     }
-  };
+  }
   return count;
 }
 //3.  sum selected
@@ -24,7 +24,7 @@ function sumSelected (options) {
     if (options[i].selected === true) {
       total += parseInt(options[i].value);
     }
-  };
+  }
   return total;
 }
 //4. average all the prices
@@ -32,7 +32,7 @@ function averagePrices (options) {
   var total = 0;
   for (var i = 0; i < options.length; i++) {
     total += parseInt(options[i].value);
-  };
+  }
   return total/(options.length);
 }
 //5. average selected prices
@@ -40,18 +40,19 @@ function averageSelected (options) {
   var total = 0;
   var numberOfSelections = 0;
   for (var i = 0; i < options.length; i++) {
-    if (options[i].selected == true) {
+    if (options[i].selected === true) {
      total += parseInt(options[i].value);
-     numberOfSelections ++
+     numberOfSelections ++;
     }
-  };
+  }
   return total/(numberOfSelections);
 }
 
 
 ///names
 
-//1. show all names, take in a string, return an array; input string of names ==> output arrays
+//1. show all names, take in a string, return an array; input string of names
+//==> output arrays
 function stringToArray (string) {
   var namesListArray  = string.split("\n");
   return namesListArray;
